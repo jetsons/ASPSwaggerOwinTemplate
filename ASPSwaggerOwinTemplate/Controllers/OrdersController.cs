b93f5ec76a3f3a4b4319ac20a09aefe5d4971546
Jetsons <info@github.com>
2019-04-24 20:05:12 +0530

    first version

---
+++ ASPSwaggerOwinTemplate/Controllers/OrdersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASPSwaggerOwinTemplate.Controllers
+{
+    [RoutePrefix("api/v1")]
+    public class OrdersController : ApiController {
+
+		[AllowAnonymous]
+		[HttpGet]
+		[Route("orders/{orderId}")]
+		public IHttpActionResult GetOrder([FromUri]string orderId) {
+			return Ok();
+		}
+
+		[AllowAnonymous]
+		[HttpGet]
+		[Route("orders/")]
+		public IHttpActionResult ListAllOrders() {
+			return Ok();
+		}
+
+		[Authorize(Roles = "Admin")]
+		[HttpPut]
+		[Route("order/{orderId}")]
+		public IHttpActionResult CreateOrder([FromUri]string orderId) {
+			return Ok();
+		}
+
+
+		[Authorize(Roles = "Admin")]
+		[HttpDelete]
+		[Route("order/{orderId}")]
+		public IHttpActionResult DeleteOrder([FromUri]string orderId) {
+			return Ok();
+		}
+
+	}
+}
---
+++ ASPSwaggerOwinTemplate/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Owin;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Swashbuckle.Application;
+
+[assembly: OwinStartup(typeof(ASPSwaggerOwinTemplate.Startup))]
+
+namespace ASPSwaggerOwinTemplate {
+	public class Startup {
+
+		public virtual void Configuration(IAppBuilder app) {
+
+			HttpConfiguration config = new HttpConfiguration();
+
+
+			// Configure REST API
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "v1/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+
+			// Configure JSON Formatter for REST API
+			var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+			jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+
+			// Configure CORS to allow JavaScript clients from any
+			// domain to access our REST API
+			app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+
+			// Configure Swagger via Swashbuckle
+			config
+			.EnableSwagger(
+				c => {
+					c.SingleApiVersion("v1", "ASPSwaggerOwinTemplate");
+				}
+			)
+			.EnableSwaggerUi();
+
+
+			// Configure REST API
+			app.UseWebApi(config);
+
+
+		}
+
+	}
+}
